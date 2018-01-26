19f770ef7ffcf817694801b3007febee259936bc
unknown <liliana.quyen.tang@gmail.com>
2018-01-26 13:34:08 -0700

    syntax error it said - joe can you have a look

--- index.php
+++ index.php
@@ -1,40 +1,58 @@
 <?php 
 session_start();
-require("include_php/connection.php");
+$_SESSION['message'] = "";
 
-//check if the user pressed 'submit'
 
-if (isset($_POST)['username']) and isset($_POST)['password'])) {
+class Check_User {
+	private $connection;
+
+
+	function __construct() {
+		require_once 'C:\wamp64\www\TeamGreen\include_php\connection.php';
+
+		$db = new DB_Connect();
+		$this->connection = $db->connect();
+
+	}
+
+
+	/*check if the credentials are correct */
+	function check_credentials() {
+		if ((isset($_POST["username"])) and (isset($_POST["password"]))) {
+	
 	//assign posted values to variables
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+			$username = $_POST['username'];
+			$password = $_POST['password'];
 
 	//check if values exist in the database
-	$query = "SELECT * FROM `users` WHERE username = '$username' and password = `password`";
+			$query = "SELECT * FROM `users` WHERE username = '$username' and password = `$password`";
 
-	$result = mysqli_query($connection, $query) or die(mysql_error($connection));
-	$count = mysqli_num_rows($result);
+			$result = mysqli_query($connection, $query) or die(mysql_error($connection));
+			$count = mysqli_num_rows($result);
 
+			if ($count == 1){
+				$_SESSION['username'] = $username;
+				$_SESSION['message'] = "Logged in sucessfully";
+			} else {
+				$_SESSION['message']= "Invalid Login Credentials";
+			}
+		}
 
-	if ($count == 1){
-		$_SESSION['username'] = $username;
-	} else {
-		$msg = "Invalid Login Credentials";
+		if (isset($_SESSION['username'])) {
+			$username = $_SESSION['username'];
+			echo "Hi . $username . ";
+			echo "This is Members Area";
+			echo "<a href='logout.php'>logout</a>";
+		}
+
+		$this->connection->close();
 	}
 }
-
-if (isset($_SESSION['username'])){
-	$username = $_SESSION['username'];
-	echo "Hi . $username . ";
-	echo "This is Members Area";
-	echo "<a href='logout.php'>logout</a>";
-
-}
-else {
-
+$credential = new Check_User();
+$credential->check_credentials();
 ?>
 
-<html>
+	<html>
 
 	<head>
 	
@@ -53,23 +71,22 @@ else {
 		<img id = "logo" src="media/portal-logo.png" alt="Caraway Facilitation Portal">
 	
 		<!-- Login Form -->
-		<form id = "login-form" method="post" action="">
-		
+		<form id = "login-form" action="login.php" method="post" autocomplete="off">
+		<?= $_SESSION['message']; ?>
 			
 			<label id = "id-label" > User ID: </label>
 			<br>
 
-			<input id = "id-input" type = "text" name="username"> </input>
+			<input id = "id-input" type = "text" name="username" required> </input>
 			<br>
 			<label id = "password-label"> Password: </label>
 			<br>
-			<input id = "password-input" type = "password" name="password"> </input>
+			<input id = "password-input" type = "password" name="password" required> </input>
 			<br>
-			<input type="submit" value="Submit"> </input>
+			<input type="submit" value="Submit" name="Submit"> </input>
 
 			<br>
 		</form>
 		
 	</body>
 </html>
-<?php } ?>
