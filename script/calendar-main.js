42828a28701561f6770272de4b6721f04b825a98
Komal Aheer <aheerk@mymacewan.ca>
2018-02-02 00:30:32 -0700

    Fix majority of calendar issues - still work to be done

--- script/calendar-main.js
+++ script/calendar-main.js
@@ -106,9 +106,14 @@ SchedulePlan.prototype.initEvents = function() {
 
 	this.singleEvents.each(function(){
 		//create the .event-date element for each event
-		var durationLabel = '<span class="event-date">'+$(this).data('start')+' - '+$(this).data('end')+'</span>';
+		/* var durationLabel = '<span class="event-date">'+$(this).data('start')+' - '+$(this).data('end')+'</span>';
 		
-		$(this).children('a').prepend($(durationLabel));
+		$(this).children('a').prepend($(durationLabel)); */
+
+		$(this)
+			.children('.event-date')
+			.html($(this).data('start')+' - '+$(this).data('end'))
+			.css ('text-align', 'center');
 
 		//detect click on the event and open the modal
 		$(this).on('click', 'a', function(event){
