134f592801150c668fa1fc4c9d70d915b510dd4b
Komal Aheer <aheerk@mymacewan.ca>
2018-02-06 16:22:44 -0700

    Start teacher calendar

--- script/calendar-main.js
+++ script/calendar-main.js
@@ -6,24 +6,6 @@
 * Edited by Komal 
 *--------------------------------------------------------------------------------------------------------*/
 
-// This variable will hold the names of the facilitators for this family 
-var facilitator_data;
-
-/* Temporary - family id will always be 1 - NEED COOKIES ! */	
-var id = 1;
-
-/* This call queries the database and updates the list of facilitators based on who's logged in */
-$.post("../include_php/calendar-get-facilitators.php", { input: id }, function (data) 
-	{ 
-		// Take the data from the database for this family, and store it in an array
-		facilitator_data = data.split(",");
-		
-		// Get rid of the empty entry at the end
-		facilitator_data.pop();
-		
-	}
- ); 
- 
  
 /* This call queries the database and updates the list of classrooms to populate the class selector */
 $.post("../include_php/get-classes.php",function (data) 
