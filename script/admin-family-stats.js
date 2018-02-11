d7e0f306494d06ccd38a803ae958c07d44d51c2d
Weso55 <olsonw7@mymacewan.ca>
2018-02-10 18:40:15 -0700

    finished default html load based on default values in selectors-> js file.for html added a few id to family information and removed some hard coded html

--- script/admin-family-stats.js
+++ script/admin-family-stats.js
@@ -109,7 +109,7 @@ $.post("../include_php/get-admin-stats-family.php",function (data)
 			);
 
 			/*
-			* updates HTML table based on default values.
+			* updates HTML table based on default values of selectors -> family,month,year.
 			*
 			*/
 
@@ -120,37 +120,49 @@ $.post("../include_php/get-admin-stats-family.php",function (data)
 			var year_selector = $('#year-selection').val();
 			console.log(year_selector);
 
-			$.post("../include_php/get-admin-stats-family-students.php", {f_id: family_id} ,function (data) 
+			$.post("../include_php/get-admin-stats-family-history.php", {f_id: family_id, month: month_selector, year: year_selector} ,function (data) 
 			{
-			//gets familitators based on family_id
-			//console.log(data);
-			//var students = data.split(",");
-			//students.pop();
 
-			//find un-ordered list tag
-			//s_list = $("#Students-list");
+			/*Find the Table selector*/
+			var table_selector = $("#stats-table");
 
+			/*extract weekly data -> creates array of strings with weekly data information inside in string*/
+			var weekly_data = data.split("~");
+			weekly_data.pop();
 
-			//for (var i = 0; i < students.length; i ++){
+			//month totaly to add to family information.
+			var monthly_total = 0;
 
-				// Create a new facilitator to add to info list
-			//	var add_student = $("<li>" + students[i] + "</li>");
-					
-				// Add facilitator name to info list 
-			//	s_list.append(add_student);
-			//}
+			/*Go through the weekly data retrieved from the database and populate html table*/
+			for (var i = 0; i < weekly_data.length; i ++){
+				/*select the table body to add onto.*/
+				var table = $("#stats-table-body");
+				// Extract weekyly info
+				var weekly_info = weekly_data[i].split(",");
 
-			//end of callback function
+				monthly_total += parseInt(weekly_info[3]);
+
+				/*before building html string to add to table check if weekly hours are completed*/
+				/*completed > required -> if statement*/
+				if (parseInt(weekly_info[2]) > parseInt(weekly_info[3]) ){
+
+					var add_week = $("<tr><td>" + weekly_info[0] + " to " + weekly_info[1] + "</td><td>" + weekly_info[3] + "</td><td>"  + "&#10003;" + "</td></tr");
+				}
+				else{
+					var add_week = $("<tr><td>" + weekly_info[0] + " to " + weekly_info[1] + "</td><td>" + weekly_info[3] + "</td><td>"  + "&#10005;" + "</td></tr");
+				}
+				table.append(add_week);
+			} // end of for loop
+
+			console.log(monthly_total);
+
+			//find selector for monthly total and update it.
+			month_span = $('#monthly-total');
+			month_span.append(monthly_total);
+			
 			}
 			// end of fifth post request
 			);
-
-
-
-
-
-
-
 
 		//end of second post call back function
 		}
@@ -164,53 +176,7 @@ $.post("../include_php/get-admin-stats-family.php",function (data)
 
 
 
-/*test case when family name is selected and January and year is selected.*/
-var table_data = "2017/01/01 2017/01/07 1.0 1.2,2017/01/08 2017/01/14 3 1.2,2017/01/15 2017/01/21 0.5 1.2,2017/01/22 2017/01/28 3.5 1.2,";
 
-		/*Find the Table selector*/
-		var table_selector = $("#stats-table");
-
-		/*extract weekly data -> creates array of strings with weekly data information inside in string*/
-		var weekly_data = table_data.split(",");
-		weekly_data.pop();
-
-		/*Go through the weekly data retrieved from the database and populate html table*/
-		for (var i = 0; i < weekly_data.length; i ++){
-
-			/*select the table body to add onto.*/
-			var table = $("#stats-table-body");
-			// Extract weekyly info
-			var weekly_info = weekly_data[i].split(" ");
-
-			/*before building html string to add to table check if weekly hours are completed*/
-			/*completed > required -> if statement*/
-			if (parseInt(weekly_info[2]) > parseInt(weekly_info[3]) ){
-
-				var add_week = $("<tr><td>" + weekly_info[0] + " to " + weekly_info[1] + "</td><td>" + weekly_info[2] + "</td><td>"  + "&#10003;" + "</td></tr");
-			}
-			else{
-				var add_week = $("<tr><td>" + weekly_info[0] + " to " + weekly_info[1] + "</td><td>" + weekly_info[2] + "</td><td>"  + "&#10005;" + "</td></tr");
-			}
-
-			console.log(add_week.html());
-			table.append(add_week);
-
-		}
-
-
-		/*
-		<script>
-			var table = $("#stats-table-body");
-			var add_week = $("<tr><td>jan 34 to feb 34</td><td>jan 34 to feb 34</td><td>jan 34 to feb 34</td></tr");
-			console.log(add_week.html());
-			table.append(add_week);
-				
-
-
-		</script>>
-
-
-		*/
 
 
 
