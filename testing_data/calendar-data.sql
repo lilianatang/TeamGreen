0f28a5aa47c61b15163bf8708d9220f5f5594ba5
Komal Aheer <aheerk@mymacewan.ca>
2018-02-12 20:13:22 -0700

    Add testing data for calendars

---
+++ testing_data/calendar-data.sql
@@ -0,0 +1,177 @@
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-12', '2018-02-12 08:45:00', '2018-02-12 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-13', '2018-02-13 08:45:00', '2018-02-13 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-14', '2018-02-14 08:45:00', '2018-02-14 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-15', '2018-02-15 08:45:00', '2018-02-15 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-16', '2018-02-16 08:45:00', '2018-02-16 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-12', '2018-02-12 08:45:00', '2018-02-12 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-13', '2018-02-13 08:45:00', '2018-02-13 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-14', '2018-02-14 08:45:00', '2018-02-14 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-15', '2018-02-15 08:45:00', '2018-02-15 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-16', '2018-02-16 08:45:00', '2018-02-16 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-19', '2018-02-19 08:45:00', '2018-02-19 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-20', '2018-02-20 08:45:00', '2018-02-20 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-21', '2018-02-21 08:45:00', '2018-02-21 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-22', '2018-02-22 08:45:00', '2018-02-22 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-23', '2018-02-23 08:45:00', '2018-02-23 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-19', '2018-02-19 08:45:00', '2018-02-19 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-20', '2018-02-20 08:45:00', '2018-02-20 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-21', '2018-02-21 08:45:00', '2018-02-21 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-22', '2018-02-22 08:45:00', '2018-02-22 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-23', '2018-02-23 08:45:00', '2018-02-23 12:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-12', '2018-02-12 11:50:00', '2018-02-12 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-13', '2018-02-13 11:50:00', '2018-02-13 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-14', '2018-02-14 11:50:00', '2018-02-14 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-15', '2018-02-15 11:50:00', '2018-02-15 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-16', '2018-02-16 11:50:00', '2018-02-16 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-12', '2018-02-12 11:50:00', '2018-02-12 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-13', '2018-02-13 11:50:00', '2018-02-13 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-14', '2018-02-14 11:50:00', '2018-02-14 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-15', '2018-02-15 11:50:00', '2018-02-15 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-16', '2018-02-16 11:50:00', '2018-02-16 13:00:00', 3);
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-19', '2018-02-19 11:50:00', '2018-02-19 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-20', '2018-02-20 11:50:00', '2018-02-20 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-21', '2018-02-21 11:50:00', '2018-02-21 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-22', '2018-02-22 11:50:00', '2018-02-22 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-23', '2018-02-23 11:50:00', '2018-02-23 13:00:00', 3);
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-19', '2018-02-19 11:50:00', '2018-02-19 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-20', '2018-02-20 11:50:00', '2018-02-20 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-21', '2018-02-21 11:50:00', '2018-02-21 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-22', '2018-02-22 11:50:00', '2018-02-22 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-23', '2018-02-23 11:50:00', '2018-02-23 13:00:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-12', '2018-02-12 12:50:00', '2018-02-12 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-13', '2018-02-13 12:50:00', '2018-02-13 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-14', '2018-02-14 12:50:00', '2018-02-14 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-15', '2018-02-15 12:50:00', '2018-02-15 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-16', '2018-02-16 12:50:00', '2018-02-16 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-12', '2018-02-12 12:50:00', '2018-02-12 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-13', '2018-02-13 12:50:00', '2018-02-13 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-14', '2018-02-14 12:50:00', '2018-02-14 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-15', '2018-02-15 12:50:00', '2018-02-15 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-16', '2018-02-16 12:50:00', '2018-02-16 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-19', '2018-02-19 12:50:00', '2018-02-19 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-20', '2018-02-20 12:50:00', '2018-02-20 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-21', '2018-02-21 12:50:00', '2018-02-21 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-22', '2018-02-22 12:50:00', '2018-02-22 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (1, '2018-02-23', '2018-02-23 12:50:00', '2018-02-23 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-19', '2018-02-19 12:50:00', '2018-02-19 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-20', '2018-02-20 12:50:00', '2018-02-20 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-21', '2018-02-21 12:50:00', '2018-02-21 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-22', '2018-02-22 12:50:00', '2018-02-22 15:45:00', 3);
+
+INSERT into facilitation_times (classroom_id, date_scheduled, time_start, time_end, facilitators_needed) 
+VALUES (2, '2018-02-23', '2018-02-23 12:50:00', '2018-02-23 15:45:00', 3);
